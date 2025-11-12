8dba7021b6380df352037209a5569f65f157b29f
AT12D19236 <yrue.m1125@gmail.com>
2025-11-13 00:49:10 +0900

    左上にフロア番号を追加 Game.h PlayerCollisionHandle に currentStage の値を増やす処理 ( 41行目～ ) を追加 CreateUI() に stageEntity ( 238行目～ ) を追加                 updater->stageTextEntity_( 313行目～ ) を追加
    SenesUIController.h
    components/StageComponents.h( 10行目 ) を インクルード
    GameUIUpdater に stageTextEntity_ ( 21行目 ) を追加
    フロアの値の更新処理をする関数ForEach<StageProgress>
    を追加 ( 65行目～ )

--- include/scenes/Game.h
+++ include/scenes/Game.h
@@ -38,7 +38,7 @@ struct PlayerCollisionHandler : ICollisionHandler {
             w.ForEach<GameStats>([](Entity e, GameStats &stats) { stats.score += 10; });
         }
         if (w.Has<GoalTag>(other)) {
-            w.ForEach<StageProgress>([](Entity e, StageProgress &sp){ sp.requestAdvance = true; });
+            w.ForEach<StageProgress>([](Entity e, StageProgress &sp) { sp.requestAdvance = true, sp.currentStage += 1; });
             DEBUGLOG("プレイヤーがゴールに到達");
         }
     }
@@ -157,7 +157,7 @@ class GameScene : public IScene {
         titleFormat.fontFamily = L"メイリオ";
         titleFormat.style = DWRITE_FONT_STYLE_ITALIC;
         titleFormat.alignment = DWRITE_TEXT_ALIGNMENT_JUSTIFIED;
-        buttonFormat.paragraphAlignment = DWRITE_PARAGRAPH_ALIGNMENT_FAR;
+        titleFormat.paragraphAlignment = DWRITE_PARAGRAPH_ALIGNMENT_FAR;
         textSystem_.CreateTextFormat("title", titleFormat);
 
     }
@@ -234,6 +234,22 @@ class GameScene : public IScene {
                                .With<UIText>(fpsText)
                                .Build();
         ownedEntities_.push_back(fpsEntity);
+
+        UITransform stageTransform;
+        stageTransform.position = {150.0f, 120.0f};
+        stageTransform.size = {130.0f, 40.0f};
+        stageTransform.anchor = {0.0f, 0.0f};
+        stageTransform.pivot = {1.0f, 0.0f};
+
+        UIText stageText{L"FLOOR: 1"};
+        stageText.color = {1.0f, 0.5f, 0.0f, 1.0f};
+        stageText.formatId = "hud";
+
+        Entity stageEntity = world.Create()
+                                 .With<UITransform>(stageTransform)
+                                 .With<UIText>(stageText)
+                                 .Build();
+        ownedEntities_.push_back(stageEntity);
         
         UIText titleText[2];
 
@@ -294,6 +310,7 @@ class GameScene : public IScene {
             updater->timeTextEntity_ = timeEntity;
             updater->fpsTextEntity_ = fpsEntity;
             updater->pauseTextEntity_ = pauseEntity;
+            updater->stageTextEntity_ = stageEntity;
         }
         ownedEntities_.push_back(uiUpdater);
     }
